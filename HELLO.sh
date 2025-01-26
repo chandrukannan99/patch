dae5662b062a2fe135e20559e9deed924edf2542
chandrukannan99 <56425515+chandrukannan99@users.noreply.github.com>
2025-01-26 16:18:10 +0530

    Update HELLO.sh

--- HELLO.sh
+++ HELLO.sh
@@ -1,2 +1,12 @@
-echo "Hello world"
-sh "cd /tmp; mkdir exec"
+pipeline {
+    agent any
+    stages {
+        stage('Run Shell') {
+            steps {
+                sh 'echo "Hello from Jenkins!"'
+                sh ' cd /tmp; mkdir exec'
+            }
+        }
+    }
+}
+
